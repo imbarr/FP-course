ddfc979d202e8b093095fdc1ec33b7245cce1d51
imbar <imbrkvskh@gmail.com>
2019-06-02 15:28:13 +0500

    practice 5

--- practice05_pub-master/src/main/scala/exercises/Eq.scala
+++ practice05_pub-master/src/main/scala/exercises/Eq.scala
@@ -1,10 +1,12 @@
 package exercises
 
+import org.scalatest.{FlatSpec, NonImplicitAssertions}
+
 /**
   * Реализуйте инстансы для typeclass Eq в объекте EqInstances:
-  *  -- для примитивных типов (обязательно должна быть реализация для boolean, int, long)
-  *  -- для класса exercises.Rational
-  *  -- для классов для всех возможных их комбинаций в Option, List, Map
+  * -- для примитивных типов (обязательно должна быть реализация для boolean, int, long)
+  * -- для класса exercises.Rational
+  * -- для классов для всех возможных их комбинаций в Option, List, Map
   *
   * Определите синтаксис для typeclass Eq в объекте EqSyntax.
   *
@@ -25,22 +27,66 @@ trait Eq[A] {
 }
 
 object EqInstances {
-}
+  import EqSyntax._
 
-object EqSyntax {
-  implicit class EqOps[A](val a: A) extends AnyVal {
+  class BasicEq[T] extends Eq[T] {
+    override def eqv(a: T, b: T): Boolean = a == b
+  }
 
+  implicit val intEq = new BasicEq[Int]
+  implicit val booleanEq = new BasicEq[Boolean]
+  implicit val longEq = new BasicEq[Long]
+
+  implicit val rationalEq: Eq[Rational] = (a: Rational, b: Rational) => {
+    a.numerator * b.denumerator === a.denumerator * b.numerator
+  }
+
+  implicit def optionEq[T: Eq]: Eq[Option[T]] = (a: Option[T], b: Option[T]) =>
+    (a, b) match {
+      case (None, None) => true
+      case (Some(first), Some(second)) => first === second
+      case _ => false
+    }
+
+  implicit def listEq[T: Eq]: Eq[List[T]] = (a: List[T], b: List[T]) =>
+    a.length == b.length && (a zip b).forall { case (first, second) => first === second }
+
+  implicit def mapEq[K: Eq, V: Eq]: Eq[Map[K, V]] = (a: Map[K, V], b: Map[K, V]) => {
+    def subset(a: Map[K, V], b: Map[K, V]): Boolean =
+      a.forall { case (key, value) => b.get(key) === Some(value) }
+
+    subset(a, b) && subset(b, a)
   }
 }
 
-object Tests {
+object EqSyntax {
+
+  implicit class EqOps[A: Eq](val a: A) {
+    def ===(other: A): Boolean = implicitly[Eq[A]].eqv(a, other)
+
+    def eqv(other: A): Boolean = a === other
+
+    def !==(other: A): Boolean = !(a === other)
+  }
+
+}
+
+class EqTests extends FlatSpec with NonImplicitAssertions {
+
   import EqInstances._
   import EqSyntax._
 
-  //1 eqv 1 // возвращает true
-  //1 === 2 // возвращает false
-  //1 !== 2 // возвращает true
-  //1 === "some-string" // не компилируется
-  //1 !== Some(2) // не компилируется
-  //List(true) === List(true) // возвращает true
+  "Eq" should "work like in the example" in {
+    assert(1 eqv 1)
+    assert(!(1 === 2))
+    assert(1 !== 2)
+    assertDoesNotCompile(""" 1 === "some-string" """)
+    assertDoesNotCompile(""" 1 !== Some(2) """)
+    assert(List(true) === List(true))
+  }
+
+  it should "work with another examples" in {
+    assert(Map(1 -> 2L, 2 -> 3L) === Map(1 -> 2L, 2 -> 3L))
+    assert(Map(1 -> 2L, 2 -> 3L) !== Map(1 -> 2L, 2 -> 4L))
+  }
 }
--- practice05_pub-master/src/main/scala/exercises/RationalOrderings.scala
+++ practice05_pub-master/src/main/scala/exercises/RationalOrderings.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import org.scalatest.FlatSpec
+
 /**
   * Для сортировки коллекций в стандартной библиотеке scala используются traits scala.math.Ordering,
   * scala.math.Ordered. Изучите как устроены эти трейты и как они используются для сортировки на примере
@@ -11,14 +13,35 @@ package exercises
   */
 object RationalOrderings {
 
-  val naturalAscendingOrdering: Ordering[Rational] = ???
+  val naturalAscendingOrdering: Ordering[Rational] = (a: Rational, b: Rational) =>
+    a.numerator * b.denumerator - a.denumerator * b.numerator
 
-  val naturalDescendingOrdering: Ordering[Rational] = ???
+  val naturalDescendingOrdering: Ordering[Rational] = naturalAscendingOrdering.reverse
 
-  val absoluteAscendingOrdering: Ordering[Rational] = ???
+  val absoluteAscendingOrdering: Ordering[Rational] = (a: Rational, b: Rational) =>
+    math.abs(a.numerator * b.denumerator) - math.abs(a.denumerator * b.numerator)
 
+  def descNaturalSort(xs: List[Rational]): List[Rational] =
+    xs.sortWith(naturalDescendingOrdering.lt)
 
-  def descNaturalSort(xs: List[Rational]): List[Rational] = ???
+  def ascAbsoluteSort(xs: List[Rational]): List[Rational] =
+    xs.sorted(absoluteAscendingOrdering)
+}
 
-  def ascAbsoluteSort(xs: List[Rational]): List[Rational] = ???
+class RationalOrderingsTests extends FlatSpec {
+  import RationalOrderings._
+
+  val list = List(Rational(1, 3), Rational(1, 2), Rational(1, 1), -Rational(1, 4))
+
+  "descNaturalSort" should "sort list" in {
+    val expected = List(Rational(1, 1), Rational(1, 2), Rational(1, 3), -Rational(1, 4))
+    val actual = descNaturalSort(list)
+    assert(expected == actual)
+  }
+
+  "ascAbsoluteSort" should "sort list" in {
+    val expected = List(-Rational(1, 4), Rational(1, 3), Rational(1, 2), Rational(1, 1))
+    val actual = ascAbsoluteSort(list)
+    assert(expected == actual)
+  }
 }
