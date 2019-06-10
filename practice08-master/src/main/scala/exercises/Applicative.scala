337e5579a9f4723112a75f8b5614024994fc5e7f
imbar <imbrkvskh@gmail.com>
2019-06-10 19:59:22 +0500

    task 8

--- practice08-master/src/main/scala/exercises/Applicative.scala
+++ practice08-master/src/main/scala/exercises/Applicative.scala
@@ -6,34 +6,53 @@ import scala.language.higherKinds
 
 //1. Рейлизуйте функции Applicative через apply и pure
 
-@typeclass trait Applicative[F[_]] extends Functor[F] { self =>
+@typeclass trait Applicative[F[_]] extends Functor[F] {
+  self =>
   def pure[A](a: A): F[A]
 
   def apply[A, B](fa: F[A])(ff: F[A => B]): F[B]
 
-  def apply2[A, B, C](fa: F[A], fb: F[B])(ff: F[(A, B) => C]): F[C] = ???
+  def apply2[A, B, C](fa: F[A], fb: F[B])(ff: F[(A, B) => C]): F[C] = {
+    val ffa = map(ff)(_.curried)
+    val ffb = apply(fa)(ffa)
+    apply(fb)(ffb)
+  }
 
-  override def map[A, B](fa: F[A])(f: A => B): F[B] = ???
+  override def map[A, B](fa: F[A])(f: A => B): F[B] =
+    apply(fa)(pure(f))
 
-  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] = ???
+  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] = {
+    val ffb = map(fa)(f.curried)
+    apply(fb)(ffb)
+  }
 
-  def map3[A, B, C, Z](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] =
-    ???
+  def map3[A, B, C, Z](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] = {
+    val ffc = map2(fa, fb)(f.curried(_)(_))
+    apply(fc)(ffc)
+  }
 
   def map4[A, B, C, D, Z](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(
-      f: (A, B, C, D) => Z): F[Z] = ???
+    f: (A, B, C, D) => Z): F[Z] = {
+    val ffd = map3(fa, fb, fc)(f.curried(_)(_)(_))
+    apply(fd)(ffd)
+  }
 
-  def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
+  def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((_, _))
 
-  def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] = ???
+  def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] =
+    map3(fa, fb, fc)((_, _, _))
 
-  def flip[A, B](ff: F[A => B]): F[A] => F[B] = ???
+  def flip[A, B](ff: F[A => B]): F[A] => F[B] =
+    apply(_)(ff)
 
-  def compose[G[_]](
-      implicit G: Applicative[G]): Applicative[Lambda[X => F[G[X]]]] =
+  def compose[G[_]](implicit G: Applicative[G]): Applicative[Lambda[X => F[G[X]]]] =
     new Applicative[Lambda[X => F[G[X]]]] {
-      def pure[A](a: A): F[G[A]] = ???
+      def pure[A](a: A): F[G[A]] = self.pure(G.pure(a))
 
-      def apply[A, B](fga: F[G[A]])(ff: F[G[A => B]]): F[G[B]] = ???
+      def apply[A, B](fga: F[G[A]])(ff: F[G[A => B]]): F[G[B]] = {
+        val transform = self.map(ff)(G.flip)
+        self.apply(fga)(transform)
+      }
     }
 }
--- practice08-master/src/main/scala/exercises/Applicatives.scala
+++ practice08-master/src/main/scala/exercises/Applicatives.scala
@@ -7,9 +7,13 @@ object Applicatives {
     //2. Рейлизуйте все функции аппликатива Option
     implicit val optionApp: Applicative[Option] =
       new Applicative[Option] {
-        def pure[A](a: A): Option[A] = ???
+        def pure[A](a: A): Option[A] = Some(a)
 
-        def apply[A, B](fa: Option[A])(ff: Option[A => B]): Option[B] = ???
+        def apply[A, B](fa: Option[A])(ff: Option[A => B]): Option[B] =
+          for {
+            a <- fa
+            f <- ff
+          } yield f(a)
       }
   }
 
@@ -17,9 +21,13 @@ object Applicatives {
 
     //3. Рейлизуйте все функции аппликатива List
     implicit val listApp: Applicative[List] = new Applicative[List] {
-      def pure[A](a: A): List[A] = ???
+      def pure[A](a: A): List[A] = List(a)
 
-      def apply[A, B](fa: List[A])(ff: List[A => B]): List[B] = ???
+      def apply[A, B](fa: List[A])(ff: List[A => B]): List[B] =
+        for {
+          a <- fa
+          f <- ff
+        } yield f(a)
     }
   }
 }
